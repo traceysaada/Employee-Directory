9a26af8f80508f03de4edfb7249694a0a017102a
Tracey Saada <tracey.saada@outlook.com>
2021-03-28 16:17:20 +0100

    Updates

--- src/utils/API.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import axios from "axios";
-
-export default {
-  getUsers: function () {
-    return axios.get("https://randomuser.me/api/?results=50");
-  },
-};
